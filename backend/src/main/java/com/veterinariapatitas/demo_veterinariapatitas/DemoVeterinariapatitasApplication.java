cc15cc92496a0803065e31407c5ce9c002628a6f
EsmeNava <ifglitterthen@gmail.com>
2025-12-14 05:11:12 -0600

    Initial env setup

---
+++ backend/src/main/java/com/veterinariapatitas/demo_veterinariapatitas/DemoVeterinariapatitasApplication.java
@@ -0,0 +1,13 @@
+package com.veterinariapatitas.demo_veterinariapatitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoVeterinariapatitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoVeterinariapatitasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/veterinariapatitas/demo_veterinariapatitas/DemoVeterinariapatitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veterinariapatitas.demo_veterinariapatitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoVeterinariapatitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
